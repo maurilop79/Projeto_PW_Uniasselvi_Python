5bcf5360250d04e79ad1b4926e95692814e18034
Maurício Lopes <108186227+maurilop79@users.noreply.github.com>
2023-05-14 15:15:04 -0300

    Commit Delete e Ajax

--- app/views.py
+++ app/views.py
@@ -23,3 +23,24 @@ def view(request, pk):
     data['db'] = Jogos.objects.get(pk=pk)
     return render(request, 'view.html', data)
 
+def edit(request, pk):
+    data = {}
+    data['db'] = Jogos.objects.get(pk=pk)
+    data['form'] = JogosForm(instance=data['db'])
+    return render(request, 'form.html', data)
+
+def update(request, pk):
+    data = {}
+    data['db'] = Jogos.objects.get(pk=pk)
+    form = JogosForm(request.POST or None, instance=data['db'])
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+
+def delete(request, pk):
+    db = Jogos.objects.get(pk=pk)
+    db.delete()
+    return redirect('home')
+
+
+
